48e258c8613c2b2e62b038b1cc29d9e9547a84c6
simon <155122848+simonpotel@users.noreply.github.com>
2024-11-19 13:22:05 +0100

    click_pion event, fix vertical bordures

--- src/render.py
+++ src/render.py
@@ -1,13 +1,11 @@
 import tkinter as tk
 
-
 ref_couleurs = {
     "reine_joueur_1": "purple",
     "tours_joueur_1": "blue",
     "reine_joueur_2": "orange",
     "tours_joueur_2": "red"
 }
-
 
 class Render:
     def __init__(self, plateau):
@@ -34,8 +32,7 @@ class Render:
             self.canvas.create_line(largeur_bordure, i * hauteur_cellule, self.canvas_width,
                                     i * hauteur_cellule)  # lignes horizontales
             self.canvas.create_line(
-                i * largeur_cellule, largeur_bordure, i * largeur_cellule, self.canvas_height + 1)  # lignes verticales
-
+                i * largeur_cellule, largeur_bordure, i * largeur_cellule, self.canvas_height + 0.25)  # lignes verticales
 
         # ligne gauche verticale
         self.canvas.create_line(largeur_bordure, largeur_bordure, largeur_bordure, self.canvas_height)
@@ -54,7 +51,16 @@ class Render:
                     y = i * hauteur_cellule + margin
                     w = (j + 1) * largeur_cellule - margin
                     h = (i + 1) * hauteur_cellule - margin
-                    self.canvas.create_oval(x, y, w, h, fill=color)
+                    oval = self.canvas.create_oval(x, y, w, h, fill=color)
+                    self.canvas.tag_bind(oval, '<Button-1>', lambda event, i=i, j=j: self.click_pion(i, j))
+
+    def click_pion(self, i, j):
+        self.plateau.get_plateau()[i][j] = (None, None)
+        self.update_plateau()
+
+    def update_plateau(self):
+        self.canvas.delete("all")
+        self.draw_plateau()
 
     def run(self):
         self.root.mainloop()
