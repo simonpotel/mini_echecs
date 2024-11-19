98068bc926cd25a619b9eb8380f062a5a60802c9
simon <155122848+simonpotel@users.noreply.github.com>
2024-11-19 09:38:51 +0100

    class joueur

--- src/joueur.py
+++ src/joueur.py
@@ -1,10 +1,13 @@
+from loguru import logger 
+from src.tour import Tour
+
 class Joueur:
     def __init__(self):
         self.coordonnees_reine = (None, None)
-        self.pieces_restantes = None
+        self.tours_restantes = None
 
-    def set_pieces_restantes(self, nombre_pieces):
-        self.pieces_restantes = nombre_pieces
+    def set_tours_restantes(self, nombre_tours):
+        self.tours_restantes = nombre_tours
 
     def set_coordonnees_reine(self, x, y):
         self.coordonnees_reine = (x, y)
@@ -12,5 +15,5 @@ class Joueur:
     def get_coordonnees_reine(self):
         return self.coordonnees_reine
 
-    def get_pieces_restantes(self):
-        return self.pieces_restantes
+    def get_tours_restantes(self):
+        return self.tours_restantes
